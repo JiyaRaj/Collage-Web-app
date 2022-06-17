3ced10b0ed5824eab093844c05ccfb8ff6ad7663
JiyaRaj <86504048+JiyaRaj@users.noreply.github.com>
2022-06-17 11:50:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-var SpeechRecognition= window.webkitSpeechRecognition;
-var Recognition= new SpeechRecognition();
- 
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
 Webcam.set({
-    width: 320,
-    height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
- });
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
 
 
-function save() {
-    Recognition.start();
-
-}
-
-Recognition.onresult=  function(event) {
-    console.log(event);
-    var content=event.results[0][0].transcript;
-    document.getElementById("textbox").innerHTML=content;
-    speak();
-    Webcam.attach( '#webcam' );
-}
 
 function speak(){
-    var synth= window.speechSynthesis;
-    var speak_data= document.getElementById("textbox").innerHTML;
-    var speak_this= new SpeechSynthesisUtterance(speak_data);
-    synth.speak(speak_this);
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
 
 }
